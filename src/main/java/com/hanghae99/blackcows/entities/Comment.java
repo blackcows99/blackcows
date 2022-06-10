956bf098333e5f68da8f39a7a1bb92ff75340c2c
printf-jin <dlghwls7662@naver.com>
2022-06-10 21:00:55 +0900

    Comment 클래스 Member 연관관계 추가

--- src/main/java/com/hanghae99/blackcows/entities/Comment.java
+++ src/main/java/com/hanghae99/blackcows/entities/Comment.java
@@ -2,10 +2,12 @@ package com.hanghae99.blackcows.entities;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @Entity
 public class Comment extends TimeStamp {
@@ -21,6 +23,10 @@ public class Comment extends TimeStamp {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "POSTS_ID")
     private Posts posts;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Member member;
 }
 
 // comment_id /   content / p
