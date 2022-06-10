b4bbe9fef1933be1bcb0dfd81c6e15b8a486c800
g <>
2022-06-10 13:46:16 +0900

    backend init

---
+++ src/main/java/com/hanghae99/blackcows/BlackcowsApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae99.blackcows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackcowsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackcowsApplication.class, args);
+    }
+
+}
