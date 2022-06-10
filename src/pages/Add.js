dce8a1fc47b277eeee31e6947f116c7ef660f6ba
pshii <sjsmssk2@gmail.com>
2022-06-10 17:27:42 +0900

    test

---
+++ src/pages/Add.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Add = () => {
+    return (
+        <div>추가페이지</div>
+    )
+}
+
+export default Add;
---
+++ src/pages/Detail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Detail = () => {
+    return (
+        <div>상세페이지</div>
+    )
+}
+
+export default Detail;
